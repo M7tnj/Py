7ae4bc589024609fc11f7d4c8f5ce27f163d93aa
Hamed1116 <145001776+Hamed1116@users.noreply.github.com>
2023-10-05 18:38:22 +0330

    Update Hamed_lm_Pset3.py

--- session_0/Problem sets/Pset_3/Hamed_lm_Pset3.py
+++ session_0/Problem sets/Pset_3/Hamed_lm_Pset3.py
@@ -1,10 +1,13 @@
 dna = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
 
-cut = dna.find("GAATTC")
-first_frag = dna[:cut]
-second_frag = dna[cut:]
+cut1 = dna.find("AATTC")
+cut2 = dna.find("AATTC")
+first_frag = dna[:cut1]
+second_frag = dna[cut2:]
 print(dna)
 print(first_frag)
 print(second_frag)
 size_of_1st_frag = print(len(first_frag))
 size_of_2nd_frag = print(len(second_frag))
+print(first_frag)
+print(second_frag)

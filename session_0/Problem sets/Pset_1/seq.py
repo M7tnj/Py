35d21eacd8ee462a4be17574fee9e49c9f07cb71
Nabileh1 <144947183+Nabileh1@users.noreply.github.com>
2023-10-06 01:51:28 -0700

    Add files via upload

---
+++ session_0/Problem sets/Pset_1/seq.py
@@ -0,0 +1,9 @@
+DNAsequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+DNAsequence_A = DNAsequence.count("AT")
+DNAsequence_B =len(DNAsequence)
+DNAsequence_C = (DNAsequence_A / DNAsequence_B)*100
+
+print(DNAsequence_C)
+
+

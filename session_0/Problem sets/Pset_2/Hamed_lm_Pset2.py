cffa6fbc38c157f5e7ef8045cead149081899695
Mostafa Tamiminezhad <130439123+M7tnj@users.noreply.github.com>
2023-10-06 06:55:59 +0330

    Update Hamed_lm_Pset2.py

--- session_0/Problem sets/Pset_2/Hamed_lm_Pset2.py
+++ session_0/Problem sets/Pset_2/Hamed_lm_Pset2.py
@@ -1,3 +1,11 @@
+# add pseudocode
+# always look up for the best pythonic style (one line between input - proceccing and the output)
+
+# defining the given sequence
 sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+# replace each basepair to its complement
 complement_of_dna = sequence.replace("A","X").replace("G","Y").replace("T","A").replace("C","G").replace("X","T").replace("Y","C")
+
+# print the output
 print(complement_of_dna)

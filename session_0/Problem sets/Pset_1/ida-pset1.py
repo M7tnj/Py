e7a4c1eb9670dd943c149a1d6e85e487bc8e7506
idanry <144947025+idanry@users.noreply.github.com>
2023-10-06 03:20:40 -0700

    Add files via upload

---
+++ session_0/Problem sets/Pset_1/ida-pset1.py
@@ -0,0 +1,9 @@
+seq = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+#Counting ATs
+a = seq.count("AT")
+#Total string length
+b = len(seq)
+#Percentage
+c = ((a/b)*100)
+#Print
+print(c)

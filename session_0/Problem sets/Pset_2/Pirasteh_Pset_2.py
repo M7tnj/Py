25a4e0dfc7c86f2f253854317d63838003a2eede
Mostafa Tamiminezhad <130439123+M7tnj@users.noreply.github.com>
2023-10-06 09:14:50 +0330

    Update Pirasteh_Pset_2.py

--- session_0/Problem sets/Pset_2/Pirasteh_Pset_2.py
+++ session_0/Problem sets/Pset_2/Pirasteh_Pset_2.py
@@ -1,5 +1,9 @@
+# always check the output
+# your output will be ['T', 'G', 'A', 'C', 'T', 'A', 'G', 'C', 'T', 'A', 'A', 'T', 'G', 'C', 'A', 'T', 'A', 'T', 'C', 'A', 'T', 'A', 'A', 'A', 'C', 'G', 'A', 'T', 'A', 'G', 'T', 'A', 'T', 'G', 'T', 'A', 'T', 'A', 'T', 'A', 'T', 'A', 'G', 'C', 'T', 'A', 'C', 'G', 'C', 'A', 'A', 'G', 'T', 'A']
+
+
 dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 
-complement = [x.replace("A","t").replace("T","a").replace("G","c").replace("C","g").upper() for x in dna]
+complement = [x.replace("A","t").replace("T","a").replace("G","c").replace("C","g").upper() for x in dna] # list methods haven't been tought yet
 
 print(complement)

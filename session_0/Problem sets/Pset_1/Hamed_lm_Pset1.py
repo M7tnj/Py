3c6440fc8589aeb072bf7cf4bd162a303e7a91eb
Hamed1116 <145001776+Hamed1116@users.noreply.github.com>
2023-10-07 13:41:55 +0330

    Hamed_lm_Pset1.py

--- session_0/Problem sets/Pset_1/Hamed_lm_Pset1.py
+++ session_0/Problem sets/Pset_1/Hamed_lm_Pset1.py
@@ -1,4 +1,8 @@
 seq = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 a = seq.count("A")
-t = sec.count("T")
-at = a + t
+t = seq.count("T")
+c = seq.count("C")
+g = seq.count("G")
+at_content = (a+t)/(a+t+c+g)*100
+at_content = round(at_content)
+print (at_content)

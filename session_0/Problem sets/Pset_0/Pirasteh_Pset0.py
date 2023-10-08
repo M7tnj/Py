225f16395fd48e3f511841fc13438ade23236eef
Parham56964 <144947106+Parham56964@users.noreply.github.com>
2023-10-08 19:32:52 +0330

    Add files via upload

---
+++ session_0/Problem sets/Pset_0/Pirasteh_Pset0.py
@@ -0,0 +1,10 @@
+# Requesting mass
+m = int(input("Enter mass in kg: "))
+
+# Defining speed of light in m/s
+c = 300000000
+
+# Calculating the total energy in joules
+e = m * c ** 2
+
+print("Energy (joules) =", e)

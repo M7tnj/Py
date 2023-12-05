893f6e73bfe489447a08f58611f1d5e29c8c057c
Maryamgnt <145039638+Maryamgnt@users.noreply.github.com>
2023-12-05 14:37:45 +0330

    Add files via upload

---
+++ session_0/Problem sets/Pset_7/Maryam_Pset7.py
@@ -0,0 +1,7 @@
+def converter(emoticons):
+ moticons= emoticons.replace(":(", "🙁")
+ moticons= emoticons.replace(":)", "🙂")
+ return emoticons
+
+convert_input = input("Enter an Emoticons")
+print(convert_input)

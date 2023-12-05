90c9b4a3116e254f85007228f218ff69bd84217f
Maryamgnt <145039638+Maryamgnt@users.noreply.github.com>
2023-12-05 17:01:07 +0330

    Update Maryam_Pset7.py

--- session_0/Problem sets/Pset_7/Maryam_Pset7.py
+++ session_0/Problem sets/Pset_7/Maryam_Pset7.py
@@ -3,8 +3,8 @@ def converter(input_string):
  input_string = input_string.replace(":)", "🙂")
  return input_string
 
- def main():
- user_input = ("Enter an Emoticons:")
-result = converter(user_input)
-print(result)
-      main()
+def main():
+   user_input = input("Enter an Emoticons:")
+   result = convert(user_input)
+   print(result)
+main()
